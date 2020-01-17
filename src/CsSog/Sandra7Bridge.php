58d283f728a9b2a3927d39b19ebedff1d320952b
shaban shaame <shaban@everdreamsoft.com>
2020-01-17 17:17:12 +0100

    initial commint

---
+++ src/CsSog/Sandra7Bridge.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by EverdreamSoft.
+ * User: Shaban Shaame
+ * Date: 26.11.19
+ * Time: 11:22
+ */
+
+class System20 extends \SandraCore\System {
+
+
+    public function __construct($env = '', $install = false, $dbHost = '127.0.0.1', $db = 'sandra', $dbUsername = 'root', $dbpassword = '')
+    {
+
+        self::initDebugStack();
+        self::$pdo = new \SandraCore\PdoConnexionWrapper($dbHost, $db,$dbUsername, $dbpassword);
+        $pdoWrapper = self::$pdo ;
+
+        $prefix = '' ;
+        $this->tablePrefix = $prefix ;
+        $suffix = '';
+        if($env)
+            $suffix = '_'.$env;
+
+        $this->conceptTable = $prefix .'Concept' . $suffix;
+        $this->linkTable =  $prefix .'Link' . $suffix;
+        $this->tableReference =  $prefix .'References' . $suffix;
+        $this->tableStorage =  $prefix .'aux_dataStorage' . $suffix;
+        $this->tableConf =  $prefix .'system_configuration' . $suffix;
+
+        if ($install) $this->install();
+
+
+        $this->systemConcept = new \SandraCore\SystemConcept($pdoWrapper, self::$logger, $this->conceptTable);
+
+        $this->deletedUNID = $this->systemConcept->get('deleted');
+        //die("on system deleted ".$this->deletedUNID);
+
+        self::$logger->connectionInfo = array('Host' => $pdoWrapper->host, 'Database' => $pdoWrapper->database, 'Sandra environment' => $env);
+
+        $this->factoryManager = new \SandraCore\FactoryManager($this);
+        $this->conceptFactory = new \SandraCore\ConceptFactory($this);
+
+        $this->instanceId = rand(0,999)."-".rand(0,9999)."-".rand(0,999);
+
+
+    }
+
+
+}
